98586c501fd483c5a1840a3d874aa0104399e0eb
tmorenobernal <tmorenobernal@gmail.com>
2021-05-03 22:46:43 +0200

    Added change teacher email command

---
+++ backoffice/context/src/main/java/com/tmoreno/mooc/backoffice/teachers/commands/changeEmail/ChangeTeacherEmailCommandParams.java
@@ -0,0 +1,25 @@
+package com.tmoreno.mooc.backoffice.teachers.commands.changeEmail;
+
+import com.tmoreno.mooc.shared.command.CommandParams;
+
+public final class ChangeTeacherEmailCommandParams extends CommandParams {
+
+    private String id;
+    private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
