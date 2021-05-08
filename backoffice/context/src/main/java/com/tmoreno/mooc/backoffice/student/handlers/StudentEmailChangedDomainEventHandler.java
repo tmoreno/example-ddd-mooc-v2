b6e77316de34e3b540656683c86fc16163105e5c
tmorenobernal <tmorenobernal@gmail.com>
2021-05-08 19:11:52 +0200

    Convert to event handler

---
+++ backoffice/context/src/main/java/com/tmoreno/mooc/backoffice/student/handlers/StudentEmailChangedDomainEventHandler.java
@@ -0,0 +1,30 @@
+package com.tmoreno.mooc.backoffice.student.handlers;
+
+import com.tmoreno.mooc.backoffice.student.domain.Student;
+import com.tmoreno.mooc.backoffice.student.domain.StudentId;
+import com.tmoreno.mooc.backoffice.student.domain.StudentRepository;
+import com.tmoreno.mooc.backoffice.student.domain.events.StudentEmailChangedDomainEvent;
+import com.tmoreno.mooc.backoffice.student.domain.exceptions.StudentNotFoundException;
+import com.tmoreno.mooc.shared.domain.Email;
+import com.tmoreno.mooc.shared.events.EventHandler;
+
+public class StudentEmailChangedDomainEventHandler implements EventHandler<StudentEmailChangedDomainEvent> {
+
+    private final StudentRepository repository;
+
+    public StudentEmailChangedDomainEventHandler(StudentRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void handle(StudentEmailChangedDomainEvent event) {
+        StudentId studentId = new StudentId(event.getAggregateId());
+        Email email = event.getEmail();
+
+        Student student = repository.find(studentId).orElseThrow(() -> new StudentNotFoundException(studentId));
+
+        student.changeEmail(email);
+
+        repository.save(student);
+    }
+}
