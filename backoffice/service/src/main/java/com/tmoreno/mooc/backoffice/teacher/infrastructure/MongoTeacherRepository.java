58a66891270880db68407bf5c75ee25e823ef436
tmorenobernal <tmorenobernal@gmail.com>
2021-05-20 00:03:30 +0200

    Added mongodb support

--- backoffice/service/src/main/java/com/tmoreno/mooc/backoffice/teacher/infrastructure/MongoTeacherRepository.java
+++ backoffice/service/src/main/java/com/tmoreno/mooc/backoffice/teacher/infrastructure/MongoTeacherRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public final class MongoTeacherRepository implements TeacherRepository {
+public class MongoTeacherRepository implements TeacherRepository {
     @Override
     public void save(Teacher teacher) {
 
