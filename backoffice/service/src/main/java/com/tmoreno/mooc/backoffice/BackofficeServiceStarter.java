bd4f001208d7a682745ca0aeee8a3516c5233fa3
tmorenobernal <tmorenobernal@gmail.com>
2021-05-19 22:27:08 +0200

    Added backoffice service starter

---
+++ backoffice/service/src/main/java/com/tmoreno/mooc/backoffice/BackofficeServiceStarter.java
@@ -0,0 +1,11 @@
+package com.tmoreno.mooc.backoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackofficeServiceStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(BackofficeServiceStarter.class, args);
+    }
+}
