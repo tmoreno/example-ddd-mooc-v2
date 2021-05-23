9779421e6e55abd41291ce2ffa152822f7be1e97
tmorenobernal <tmorenobernal@gmail.com>
2021-05-23 20:55:36 +0200

    Removed unused command

--- backoffice/service/src/main/java/com/tmoreno/mooc/backoffice/teacher/configuration/TeacherSpringBeansConfiguration.java
+++ backoffice/service/src/main/java/com/tmoreno/mooc/backoffice/teacher/configuration/TeacherSpringBeansConfiguration.java
@@ -1,8 +1,7 @@
 package com.tmoreno.mooc.backoffice.teacher.configuration;
 
-import com.tmoreno.mooc.backoffice.teacher.commands.UpdateTeacherCommand;
-import com.tmoreno.mooc.backoffice.teacher.commands.changeEmail.ChangeTeacherEmailCommand;
 import com.tmoreno.mooc.backoffice.teacher.commands.CreateTeacherCommand;
+import com.tmoreno.mooc.backoffice.teacher.commands.UpdateTeacherCommand;
 import com.tmoreno.mooc.backoffice.teacher.domain.TeacherRepository;
 import com.tmoreno.mooc.shared.events.EventBus;
 import org.springframework.context.annotation.Bean;
@@ -19,10 +18,5 @@ public class TeacherSpringBeansConfiguration {
     @Bean
     public UpdateTeacherCommand updateTeacherCommand(TeacherRepository repository, EventBus eventBus) {
         return new UpdateTeacherCommand(repository, eventBus);
-    }
-
-    @Bean
-    public ChangeTeacherEmailCommand changeTeacherEmailCommand(TeacherRepository repository, EventBus eventBus) {
-        return new ChangeTeacherEmailCommand(repository, eventBus);
     }
 }
