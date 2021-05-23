98c602a8417de0947738bab3e7815cbe98ac791d
tmorenobernal <tmorenobernal@gmail.com>
2021-05-23 22:54:40 +0200

    Added more exceptions

--- backoffice/service/src/main/java/com/tmoreno/mooc/backoffice/teacher/configuration/ExceptionHelper.java
+++ backoffice/service/src/main/java/com/tmoreno/mooc/backoffice/teacher/configuration/ExceptionHelper.java
@@ -4,6 +4,11 @@ import com.tmoreno.mooc.backoffice.teacher.domain.exceptions.TeacherCourseNotFou
 import com.tmoreno.mooc.backoffice.teacher.domain.exceptions.TeacherExistsException;
 import com.tmoreno.mooc.backoffice.teacher.domain.exceptions.TeacherNotFoundException;
 import com.tmoreno.mooc.shared.domain.ExceptionResponseBody;
+import com.tmoreno.mooc.shared.domain.exceptions.InvalidDurationException;
+import com.tmoreno.mooc.shared.domain.exceptions.InvalidEmailException;
+import com.tmoreno.mooc.shared.domain.exceptions.InvalidIdentifierException;
+import com.tmoreno.mooc.shared.domain.exceptions.InvalidPersonNameException;
+import com.tmoreno.mooc.shared.domain.exceptions.MoneyValueIsNegativeException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,6 +16,20 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public final class ExceptionHelper {
+
+    @ExceptionHandler(value = {
+        InvalidDurationException.class,
+        InvalidEmailException.class,
+        InvalidIdentifierException.class,
+        InvalidPersonNameException.class,
+        MoneyValueIsNegativeException.class
+    })
+    public ResponseEntity<Object> badRequestHandler(RuntimeException e) {
+        return new ResponseEntity<>(
+                new ExceptionResponseBody(HttpStatus.BAD_REQUEST.value(), e.getMessage()),
+                HttpStatus.BAD_REQUEST
+        );
+    }
 
     @ExceptionHandler(value = {TeacherCourseNotFoundException.class, TeacherNotFoundException.class})
     public ResponseEntity<Object> notFoundHandler(RuntimeException e) {
