20e057abbfa14576c37cfb33c59e3d3ec582074e
tmorenobernal <tmorenobernal@gmail.com>
2021-05-23 22:15:25 +0200

    Added teacher get controller

--- backoffice/service/src/main/java/com/tmoreno/mooc/backoffice/teacher/configuration/TeacherSpringBeansConfiguration.java
+++ backoffice/service/src/main/java/com/tmoreno/mooc/backoffice/teacher/configuration/TeacherSpringBeansConfiguration.java
@@ -3,6 +3,8 @@ package com.tmoreno.mooc.backoffice.teacher.configuration;
 import com.tmoreno.mooc.backoffice.teacher.commands.CreateTeacherCommand;
 import com.tmoreno.mooc.backoffice.teacher.commands.UpdateTeacherCommand;
 import com.tmoreno.mooc.backoffice.teacher.domain.TeacherRepository;
+import com.tmoreno.mooc.backoffice.teacher.queries.FindTeacherQuery;
+import com.tmoreno.mooc.backoffice.teacher.queries.FindTeachersQuery;
 import com.tmoreno.mooc.shared.events.EventBus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,5 +20,15 @@ public class TeacherSpringBeansConfiguration {
     @Bean
     public UpdateTeacherCommand updateTeacherCommand(TeacherRepository repository, EventBus eventBus) {
         return new UpdateTeacherCommand(repository, eventBus);
+    }
+
+    @Bean
+    public FindTeacherQuery findTeacherQuery(TeacherRepository repository) {
+        return new FindTeacherQuery(repository);
+    }
+
+    @Bean
+    public FindTeachersQuery findTeachersQuery(TeacherRepository repository) {
+        return new FindTeachersQuery(repository);
     }
 }
