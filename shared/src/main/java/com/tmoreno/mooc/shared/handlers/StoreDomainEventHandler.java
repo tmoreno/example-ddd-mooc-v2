f114df44bef529700306e194fae4c1e169176011
tmorenobernal <tmorenobernal@gmail.com>
2021-05-24 18:03:37 +0200

    Added store domain event handler

---
+++ shared/src/main/java/com/tmoreno/mooc/shared/handlers/StoreDomainEventHandler.java
@@ -0,0 +1,19 @@
+package com.tmoreno.mooc.shared.handlers;
+
+import com.tmoreno.mooc.shared.domain.DomainEventRepository;
+import com.tmoreno.mooc.shared.events.DomainEvent;
+import com.tmoreno.mooc.shared.events.EventHandler;
+
+public final class StoreDomainEventHandler implements EventHandler<DomainEvent> {
+
+    private final DomainEventRepository repository;
+
+    public StoreDomainEventHandler(DomainEventRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void handle(DomainEvent event) {
+        repository.store(event);
+    }
+}
