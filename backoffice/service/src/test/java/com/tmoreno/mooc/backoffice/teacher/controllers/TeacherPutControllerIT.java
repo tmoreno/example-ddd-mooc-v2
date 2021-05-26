3e374a49118154f4cdca52aad6c6d4410c9ddd8d
tmorenobernal <tmorenobernal@gmail.com>
2021-05-26 19:53:21 +0200

    Added test for put method

---
+++ backoffice/service/src/test/java/com/tmoreno/mooc/backoffice/teacher/controllers/TeacherPutControllerIT.java
@@ -0,0 +1,84 @@
+package com.tmoreno.mooc.backoffice.teacher.controllers;
+
+import com.tmoreno.mooc.backoffice.BaseControllerIT;
+import com.tmoreno.mooc.backoffice.mothers.TeacherIdMother;
+import com.tmoreno.mooc.backoffice.mothers.TeacherMother;
+import com.tmoreno.mooc.backoffice.teacher.domain.Teacher;
+import com.tmoreno.mooc.backoffice.teacher.domain.TeacherRepository;
+import com.tmoreno.mooc.shared.domain.Email;
+import com.tmoreno.mooc.shared.domain.PersonName;
+import com.tmoreno.mooc.shared.mothers.EmailMother;
+import com.tmoreno.mooc.shared.mothers.PersonNameMother;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class TeacherPutControllerIT extends BaseControllerIT {
+
+    @SpyBean
+    private TeacherRepository teacherRepository;
+
+    @BeforeEach
+    public void setUp() {
+        url += "/teachers";
+    }
+
+    @Test
+    public void given_existing_teacher_when_send_put_request_then_receive_ok_response_and_teacher_is_changed_and_event_stored() {
+        Teacher teacher = TeacherMother.random();
+
+        teacherRepository.save(teacher);
+
+        PersonName name = PersonNameMother.random();
+        Email email = EmailMother.random();
+
+        Map<String, Object> request = Map.of(
+            "name", name.getValue(),
+            "email", email.getValue()
+        );
+
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(request);
+        ResponseEntity<String> response = restTemplate.exchange(url + "/" + teacher.getId().getValue(), HttpMethod.PUT, entity, String.class);
+
+        assertThat(response.getStatusCode(), is(HttpStatus.OK));
+
+        Teacher teacherPersisted = teacherRepository.find(teacher.getId()).orElseThrow();
+        assertThat(teacherPersisted.getId(), is(teacher.getId()));
+        assertThat(teacherPersisted.getName(), is(name));
+        assertThat(teacherPersisted.getEmail(), is(email));
+
+        verify(domainEventRepository, times(2)).store(any());
+    }
+
+    @Test
+    public void given_not_existing_teacher_when_send_put_request_then_receive_not_found_response_and_teacher_is_not_changed_and_event_is_not_stored() {
+        String teacherId = TeacherIdMother.random().getValue();
+
+        Map<String, Object> request = Map.of(
+            "name", PersonNameMother.random().getValue(),
+            "email", EmailMother.random().getValue()
+        );
+
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(request);
+        ResponseEntity<String> response = restTemplate.exchange(url + "/" + teacherId, HttpMethod.PUT, entity, String.class);
+
+        assertThat(response.getStatusCode(), is(HttpStatus.NOT_FOUND));
+
+        verify(teacherRepository, never()).save(any());
+
+        verify(domainEventRepository, never()).store(any());
+    }
+}
