0d94fe15d6bdfe03b554927585a849b78a9d22f1
tmorenobernal <tmorenobernal@gmail.com>
2021-05-30 23:28:40 +0200

    Generate valid dates

--- shared/src/test/java/com/tmoreno/mooc/shared/mothers/CreatedOnMother.java
+++ shared/src/test/java/com/tmoreno/mooc/shared/mothers/CreatedOnMother.java
@@ -1,10 +1,23 @@
 package com.tmoreno.mooc.shared.mothers;
 
+import com.github.javafaker.Faker;
 import com.tmoreno.mooc.shared.domain.CreatedOn;
-import org.apache.commons.lang3.RandomUtils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public final class CreatedOnMother {
     public static CreatedOn random() {
-        return new CreatedOn(RandomUtils.nextLong());
+        try {
+            SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+            Date from = format.parse("01/01/1975 00:00:00");
+            Date to = format.parse("31/12/2055 23:59:59");
+
+            return new CreatedOn(Faker.instance().date().between(from, to).getTime());
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
