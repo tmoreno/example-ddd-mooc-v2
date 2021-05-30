1ba63fd02ab8a9068b4795c8c9ae0ae769851fc7
tmorenobernal <tmorenobernal@gmail.com>
2021-05-30 18:50:52 +0200

    Added student tables

--- backoffice/service/src/main/resources/database/backoffice.sql
+++ backoffice/service/src/main/resources/database/backoffice.sql
@@ -20,3 +20,23 @@ CREATE TABLE IF NOT EXISTS teacher_courses (
     course_id CHAR(36) NOT NULL,
     PRIMARY KEY (teacher_id, course_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS students (
+    id CHAR(36) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS student_courses (
+    student_id CHAR(36) NOT NULL,
+    course_id CHAR(36) NOT NULL,
+    PRIMARY KEY (student_id, course_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS student_reviews (
+    student_id CHAR(36) NOT NULL,
+    course_id CHAR(36) NOT NULL,
+    review_id CHAR(36) NOT NULL,
+    PRIMARY KEY (student_id, course_id, review_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
