61458aa5d13572c625ebfadaf203856df80894c7
tmorenobernal <tmorenobernal@gmail.com>
2021-06-01 22:59:35 +0200

    Added course exceptions handler

--- backoffice/service/src/main/java/com/tmoreno/mooc/backoffice/configuration/ExceptionHelper.java
+++ backoffice/service/src/main/java/com/tmoreno/mooc/backoffice/configuration/ExceptionHelper.java
@@ -1,5 +1,21 @@
 package com.tmoreno.mooc.backoffice.configuration;
 
+import com.tmoreno.mooc.backoffice.course.domain.exceptions.ChangeCourseAttributeException;
+import com.tmoreno.mooc.backoffice.course.domain.exceptions.CourseExistsException;
+import com.tmoreno.mooc.backoffice.course.domain.exceptions.CourseNotFoundException;
+import com.tmoreno.mooc.backoffice.course.domain.exceptions.CourseReviewNotFoundException;
+import com.tmoreno.mooc.backoffice.course.domain.exceptions.CourseSectionClassNotFoundException;
+import com.tmoreno.mooc.backoffice.course.domain.exceptions.CourseSectionNotFoundException;
+import com.tmoreno.mooc.backoffice.course.domain.exceptions.CourseStudentNotFoundException;
+import com.tmoreno.mooc.backoffice.course.domain.exceptions.CourseTeacherNotFoundException;
+import com.tmoreno.mooc.backoffice.course.domain.exceptions.DiscardCourseException;
+import com.tmoreno.mooc.backoffice.course.domain.exceptions.InvalidCourseDescriptionException;
+import com.tmoreno.mooc.backoffice.course.domain.exceptions.InvalidCourseImageUrlException;
+import com.tmoreno.mooc.backoffice.course.domain.exceptions.InvalidCourseSummaryException;
+import com.tmoreno.mooc.backoffice.course.domain.exceptions.InvalidCourseTitleException;
+import com.tmoreno.mooc.backoffice.course.domain.exceptions.InvalidSectionClassTitleException;
+import com.tmoreno.mooc.backoffice.course.domain.exceptions.InvalidSectionTitleException;
+import com.tmoreno.mooc.backoffice.course.domain.exceptions.PublishCourseException;
 import com.tmoreno.mooc.backoffice.review.domain.exceptions.ReviewNotFoundException;
 import com.tmoreno.mooc.backoffice.student.domain.exceptions.StudentCourseNotFoundException;
 import com.tmoreno.mooc.backoffice.student.domain.exceptions.StudentNotFoundException;
@@ -30,7 +46,13 @@ public final class ExceptionHelper {
         InvalidEmailException.class,
         InvalidIdentifierException.class,
         InvalidPersonNameException.class,
-        MoneyValueIsNegativeException.class
+        MoneyValueIsNegativeException.class,
+        InvalidCourseDescriptionException.class,
+        InvalidCourseImageUrlException.class,
+        InvalidCourseSummaryException.class,
+        InvalidCourseTitleException.class,
+        InvalidSectionTitleException.class,
+        InvalidSectionClassTitleException.class
     })
     public ResponseEntity<Object> badRequestHandler(RuntimeException e) {
         logger.error("Bad request", e);
@@ -47,7 +69,13 @@ public final class ExceptionHelper {
         StudentNotFoundException.class,
         StudentCourseNotFoundException.class,
         StudentReviewNotFoundException.class,
-        ReviewNotFoundException.class
+        ReviewNotFoundException.class,
+        CourseNotFoundException.class,
+        CourseReviewNotFoundException.class,
+        CourseSectionClassNotFoundException.class,
+        CourseSectionNotFoundException.class,
+        CourseStudentNotFoundException.class,
+        CourseTeacherNotFoundException.class
     })
     public ResponseEntity<Object> notFoundHandler(RuntimeException e) {
         logger.error("Not found", e);
@@ -58,7 +86,13 @@ public final class ExceptionHelper {
         );
     }
 
-    @ExceptionHandler(TeacherExistsException.class)
+    @ExceptionHandler(value = {
+        TeacherExistsException.class,
+        CourseExistsException.class,
+        ChangeCourseAttributeException.class,
+        DiscardCourseException.class,
+        PublishCourseException.class
+    })
     public ResponseEntity<Object> preconditionFailedHandler(RuntimeException e) {
         logger.error("Precondition failed", e);
 
