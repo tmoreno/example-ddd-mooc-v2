24ea14357bfb6bc71b3c1be391bdc22b58c3d3ef
tmorenobernal <tmorenobernal@gmail.com>
2021-06-01 22:47:06 +0200

    Added course tables

--- backoffice/service/src/main/resources/database/backoffice.sql
+++ backoffice/service/src/main/resources/database/backoffice.sql
@@ -50,3 +50,48 @@ CREATE TABLE IF NOT EXISTS reviews (
     created_on DATETIME(3) NOT NULL,
     PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS courses (
+    id CHAR(36) NOT NULL,
+    title VARCHAR(500) NOT NULL,
+    image_url VARCHAR(500) NULL,
+    summary VARCHAR(1000) NULL,
+    description VARCHAR(5000) NULL,
+    state VARCHAR(15) NOT NULL,
+    language VARCHAR(20) NULL,
+    price_value DOUBLE(5, 2) NULL,
+    price_currency CHAR(3) NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS course_sections (
+    id CHAR(36) NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS course_section_classes (
+    id CHAR(36) NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    duration_in_seconds INT(4) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS course_reviews (
+    course_id CHAR(36) NOT NULL,
+    student_id CHAR(36) NOT NULL,
+    review_id CHAR(36) NOT NULL,
+    PRIMARY KEY (course_id, student_id, review_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS course_students (
+    course_id CHAR(36) NOT NULL,
+    student_id CHAR(36) NOT NULL,
+    PRIMARY KEY (course_id, student_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS course_teachers (
+    course_id CHAR(36) NOT NULL,
+    teacher_id CHAR(36) NOT NULL,
+    PRIMARY KEY (course_id, teacher_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
