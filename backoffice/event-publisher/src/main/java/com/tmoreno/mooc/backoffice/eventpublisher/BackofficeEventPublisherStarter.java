b8769f63c47da65842b0c588f7795dd061dc4e48
tmorenobernal <tmorenobernal@gmail.com>
2021-06-11 17:13:10 +0200

    Change package

--- backoffice/event-publisher/src/main/java/com/tmoreno/mooc/backoffice/eventpublisher/BackofficeEventPublisherStarter.java
+++ backoffice/event-publisher/src/main/java/com/tmoreno/mooc/backoffice/eventpublisher/BackofficeEventPublisherStarter.java
@@ -1,4 +1,4 @@
-package com.tmoreno.mooc.backoffice;
+package com.tmoreno.mooc.backoffice.eventpublisher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
