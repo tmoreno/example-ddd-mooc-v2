313dc0594c6826a66b782469c9dbe08bae6a03fd
tmorenobernal <tmorenobernal@gmail.com>
2021-06-11 17:27:46 +0200

    Added starter

---
+++ frontoffice/service/src/main/java/com/tmoreno/mooc/frontoffice/FrontofficeServiceStarter.java
@@ -0,0 +1,11 @@
+package com.tmoreno.mooc.frontoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontofficeServiceStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(FrontofficeServiceStarter.class, args);
+    }
+}
