bb57cadab7c9a36c89b1a983947dc9a7f01f5d74
tmorenobernal <tmorenobernal@gmail.com>
2021-07-10 22:22:22 +0200

    Added setter methods to enable deserialization

--- shared/src/main/java/com/tmoreno/mooc/shared/events/DomainEvent.java
+++ shared/src/main/java/com/tmoreno/mooc/shared/events/DomainEvent.java
@@ -5,23 +5,41 @@ import com.tmoreno.mooc.shared.domain.Identifier;
 import java.time.Instant;
 
 public abstract class DomainEvent {
-    private final Identifier eventId;
-    private final Instant occurredOn;
+    private Identifier eventId;
+    protected String eventName;
+    protected int version;
+    private Instant occurredOn;
 
     public DomainEvent() {
         this.eventId = new Identifier();
         this.occurredOn = Instant.now();
     }
 
-    public abstract String getEventName();
-
-    public abstract int getVersion();
-
     public String getEventId() {
         return eventId.getValue();
     }
 
+    public void setEventId(String eventId) {
+        this.eventId = new Identifier(eventId);
+    }
+
+    public abstract String getEventName();
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public abstract int getVersion();
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
     public String getOccurredOn() {
         return occurredOn.toString();
+    }
+
+    public void setOccurredOn(String occurredOn) {
+        this.occurredOn = Instant.parse(occurredOn);
     }
 }
