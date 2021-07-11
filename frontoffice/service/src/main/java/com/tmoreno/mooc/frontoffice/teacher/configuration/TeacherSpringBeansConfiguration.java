767530880b329149ae413889cc00e3e329c5c4fa
tmorenobernal <tmorenobernal@gmail.com>
2021-07-11 12:23:45 +0200

    Added teacher name changed listener and handler

--- frontoffice/service/src/main/java/com/tmoreno/mooc/frontoffice/teacher/configuration/TeacherSpringBeansConfiguration.java
+++ frontoffice/service/src/main/java/com/tmoreno/mooc/frontoffice/teacher/configuration/TeacherSpringBeansConfiguration.java
@@ -3,6 +3,7 @@ package com.tmoreno.mooc.frontoffice.teacher.configuration;
 import com.tmoreno.mooc.frontoffice.teacher.domain.TeacherRepository;
 import com.tmoreno.mooc.frontoffice.teacher.handlers.TeacherCreatedDomainEventHandler;
 import com.tmoreno.mooc.frontoffice.teacher.handlers.TeacherEmailChangedDomainEventHandler;
+import com.tmoreno.mooc.frontoffice.teacher.handlers.TeacherNameChangedDomainEventHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,5 +18,10 @@ public class TeacherSpringBeansConfiguration {
     @Bean
     public TeacherEmailChangedDomainEventHandler teacherEmailChangedDomainEventHandler(TeacherRepository repository) {
         return new TeacherEmailChangedDomainEventHandler(repository);
+    }
+
+    @Bean
+    public TeacherNameChangedDomainEventHandler teacherNameChangedDomainEventHandler(TeacherRepository repository) {
+        return new TeacherNameChangedDomainEventHandler(repository);
     }
 }
---
+++ frontoffice/service/src/main/java/com/tmoreno/mooc/frontoffice/teacher/listeners/TeacherNameChangedListener.java
@@ -0,0 +1,39 @@
+package com.tmoreno.mooc.frontoffice.teacher.listeners;
+
+import com.tmoreno.mooc.frontoffice.teacher.domain.events.TeacherNameChangedDomainEvent;
+import com.tmoreno.mooc.frontoffice.teacher.handlers.TeacherNameChangedDomainEventHandler;
+import com.tmoreno.mooc.shared.events.MoocEventListener;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public final class TeacherNameChangedListener implements MoocEventListener<TeacherNameChangedDomainEvent> {
+
+    private final TeacherNameChangedDomainEventHandler teacherNameChangedDomainEventHandler;
+
+    public TeacherNameChangedListener(TeacherNameChangedDomainEventHandler teacherNameChangedDomainEventHandler) {
+        this.teacherNameChangedDomainEventHandler = teacherNameChangedDomainEventHandler;
+    }
+
+    @Override
+    @RabbitListener(
+        bindings = @QueueBinding(
+            exchange = @Exchange(
+                value = "${infrastructure.rabbitmq.backofficeExchangeName}",
+                type = "${infrastructure.rabbitmq.backofficeExchangeType}"
+            ),
+            value = @Queue(
+                value = "frontoffice.teacher.change_name_on_teacher_name_changed",
+                durable = "false"
+            ),
+            key = "mooc.backoffice.1.teacher.name.changed"
+        ),
+        messageConverter = "jsonConverter"
+    )
+    public void on(TeacherNameChangedDomainEvent event) {
+        teacherNameChangedDomainEventHandler.handle(event);
+    }
+}
