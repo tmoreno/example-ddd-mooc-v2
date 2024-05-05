4988a2f98031780ab6d906a3140a1cbc7f82f1fd
tmoreno <tmorenobernal@gmail.com>
2024-05-05 20:08:37 +0200

    Changed docker-compose file location and change startup timeout

--- backoffice/service/src/test/java/com/tmoreno/mooc/backoffice/config/TestcontainersInitializer.java
+++ backoffice/service/src/test/java/com/tmoreno/mooc/backoffice/config/TestcontainersInitializer.java
@@ -2,16 +2,18 @@ package com.tmoreno.mooc.backoffice.config;
 
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.ComposeContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.lifecycle.Startables;
 
 import java.io.File;
+import java.time.Duration;
 
 public class TestcontainersInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
-    static DockerComposeContainer<?> dockerComposeContainer = new DockerComposeContainer<>(new File("docker-compose.yml"))
+    static ComposeContainer dockerComposeContainer = new ComposeContainer(new File("../../docker-compose.yml"))
         .withLocalCompose(true)
-        .withExposedService("backoffice-db", 3306);
+        .withExposedService("backoffice-db", 3306, Wait.forListeningPort().withStartupTimeout(Duration.ofSeconds(120)));
 
     static {
         Startables.deepStart(dockerComposeContainer).join();
