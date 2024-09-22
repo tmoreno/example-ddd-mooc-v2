b55600c6678d0374465d87ced2b89c1a366d6607
tmoreno <tmorenobernal@gmail.com>
2024-09-22 23:00:42 +0200

    New test for post course

---
+++ backoffice/service/src/test/java/com/tmoreno/mooc/backoffice/course/CoursePostControllerTest.java
@@ -0,0 +1,93 @@
+package com.tmoreno.mooc.backoffice.course;
+
+import com.tmoreno.mooc.backoffice.course.domain.Course;
+import com.tmoreno.mooc.backoffice.course.domain.CourseRepository;
+import com.tmoreno.mooc.backoffice.mothers.CourseMother;
+import com.tmoreno.mooc.backoffice.support.E2ETest;
+import com.tmoreno.mooc.backoffice.utils.DatabaseUtils;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@E2ETest
+public class CoursePostControllerTest {
+
+    private static final String ANY_COURSE_ID = UUID.randomUUID().toString();
+    private static final String ANY_COURSE_TITLE = RandomStringUtils.randomAlphabetic(10, 100);
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @BeforeEach
+    public void setUp() {
+        DatabaseUtils databaseUtils = new DatabaseUtils(jdbcTemplate);
+        databaseUtils.initialize();
+    }
+
+    @Test
+    void should_return_412_status_code_when_create_a_course_with_the_same_id_than_existing_one() throws Exception {
+        Course course = CourseMother.random();
+        courseRepository.save(course);
+
+        String content = """
+            {
+                "id": "%s",
+                "title": "%s"
+            }
+            """.formatted(course.getId().getValue(), ANY_COURSE_TITLE);
+
+        mockMvc
+            .perform(post("/courses").contentType(MediaType.APPLICATION_JSON).content(content))
+            .andExpect(status().isPreconditionFailed())
+            .andExpect(jsonPath("$.code").value("course-exists"));
+    }
+
+    @Test
+    void should_return_412_status_code_when_create_a_course_with_the_same_title_than_existing_one() throws Exception {
+        Course course = CourseMother.random();
+        courseRepository.save(course);
+
+        String content = """
+            {
+                "id": "%s",
+                "title": "%s"
+            }
+            """.formatted(ANY_COURSE_ID, course.getTitle().getValue());
+
+        mockMvc
+            .perform(post("/courses").contentType(MediaType.APPLICATION_JSON).content(content))
+            .andExpect(status().isPreconditionFailed())
+            .andExpect(jsonPath("$.code").value("course-exists"));
+    }
+
+    @Test
+    void should_return_201_status_code_when_create_a_course() throws Exception {
+        String content = """
+            {
+                "id": "%s",
+                "title": "%s"
+            }
+            """.formatted(ANY_COURSE_ID, ANY_COURSE_TITLE);
+
+        mockMvc
+            .perform(post("/courses").contentType(MediaType.APPLICATION_JSON).content(content))
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("$").doesNotExist());
+    }
+}
