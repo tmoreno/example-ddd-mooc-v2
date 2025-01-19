57693708bcbc510a490fb5443f0c111a74005f14
tmoreno <tmorenobernal@gmail.com>
2025-01-19 17:52:42 +0100

    Create CourseId class by context

---
+++ frontoffice-service/src/main/java/com/tmoreno/mooc/frontoffice/teacher/domain/CourseId.java
@@ -0,0 +1,9 @@
+package com.tmoreno.mooc.frontoffice.teacher.domain;
+
+import com.tmoreno.mooc.shared.domain.Identifier;
+
+public final class CourseId extends Identifier {
+    public CourseId(String value) {
+        super(value);
+    }
+}
