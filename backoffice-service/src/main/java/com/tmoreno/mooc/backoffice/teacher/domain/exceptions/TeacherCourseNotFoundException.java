cc5fb904598dda399dad9b5977aa1d25f50f2ac8
tmoreno <tmorenobernal@gmail.com>
2025-01-19 17:29:20 +0100

    Create teacher exceptions by context

---
+++ backoffice-service/src/main/java/com/tmoreno/mooc/backoffice/teacher/domain/exceptions/TeacherCourseNotFoundException.java
@@ -0,0 +1,14 @@
+package com.tmoreno.mooc.backoffice.teacher.domain.exceptions;
+
+import com.tmoreno.mooc.shared.domain.CourseId;
+import com.tmoreno.mooc.shared.domain.TeacherId;
+import com.tmoreno.mooc.shared.domain.exceptions.BaseDomainException;
+
+public final class TeacherCourseNotFoundException extends BaseDomainException {
+    public TeacherCourseNotFoundException(TeacherId teacherId, CourseId courseId) {
+        super(
+                "teacher-course-not-found",
+                "Course: " + courseId.getValue() + " not found in teacher: " + teacherId.getValue()
+        );
+    }
+}
---
+++ frontoffice-service/src/main/java/com/tmoreno/mooc/frontoffice/teacher/domain/exceptions/TeacherNotFoundException.java
@@ -0,0 +1,13 @@
+package com.tmoreno.mooc.frontoffice.teacher.domain.exceptions;
+
+import com.tmoreno.mooc.shared.domain.TeacherId;
+import com.tmoreno.mooc.shared.domain.exceptions.BaseDomainException;
+
+public final class TeacherNotFoundException extends BaseDomainException {
+    public TeacherNotFoundException(TeacherId teacherId) {
+        super(
+                "teacher-not-found",
+                "Teacher not found: " + teacherId.getValue()
+        );
+    }
+}
